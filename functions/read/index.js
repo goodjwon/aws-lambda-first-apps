59969513a5b107bb9813d494ce7a0a91cbca2a9f
goodjwon <jwon76@naver.com>
2018-07-10 17:04:31 +0900

    IAM 권한 내용 기술

--- functions/read/index.js
+++ functions/read/index.js
@@ -6,18 +6,35 @@ const docClient = new AWS.DynamoDB.DocumentClient({region : "ap-northeast-2"});
 
 exports.handle = function(e, ctx, cb) {
 
+  /*
   let scanningParameter = {
     TableName : 'Content',
     Limit : 100
   };
 
-  docClient.scan(scanningParameter, function(err, data){
+  docClient.scan(scanningParameter, function(err, data){  ///IAM Lambdb Function user have scan permission
     if(err){
       cb(err, null);
     }else{
       cb(null, data);
     }
   });
+*/
+
+var params = {
+  TableName : 'Content',
+  Key : {
+    "contentKey":"1234598778"
+  }
+}
+
+docClient.get(params, function(err, data){  //IAM Lambdb Function user have get permission
+  if(err){
+    cb(err, null);
+  }else{
+    cb(null, data);
+  }
+});
 
   console.log('processing event: %j', e)
 }
